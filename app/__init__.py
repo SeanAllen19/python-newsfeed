1f56c7687313186898936c7c16571ab2b5dae8ad
SeanAllen19 <saallen19.dev@gmail.com>
2023-10-05 13:07:10 -0500

    finalized the Homepage Views

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
-from app.routes import home, dashboard
 from flask import Flask
+from app.routes import home, dashboard
+from app.db import init_db
+from app.utils import filters
 
 
 def create_app(test_config=None):
@@ -18,6 +20,12 @@ def create_app(test_config=None):
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
 
+  app.jinja_env.filters['format_url'] = filters.format_url
+  app.jinja_env.filters['format_date'] = filters.format_date
+  app.jinja_env.filters['format_plural'] = filters.format_plural
 
+ 
+  init_db(app)
+  
   return app
 
--- app/models/Post.py
+++ app/models/Post.py
@@ -10,16 +10,13 @@ class Post(Base):
   title = Column(String(100), nullable=False)
   post_url = Column(String(100), nullable=False)
   user_id = Column(Integer, ForeignKey('users.id'))
+  vote_count = column_property(select(func.count(Vote.id)).where(Vote.post_id == id))
   created_at = Column(DateTime, default=datetime.now)
   updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
-  
   user = relationship('User')
   comments = relationship('Comment', cascade='all,delete')
-
-  vote_count = column_property(
-  select([func.count(Vote.id)]).where(Vote.post_id == id)
-  )
-  
   votes = relationship('Vote', cascade='all,delete')
+
+  
 
 
